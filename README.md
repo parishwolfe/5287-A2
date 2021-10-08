a3efe15f4b34debe8ef08b11380b07ff603acf7f
Parish Wolfe <parishwolfe@gmail.com>
2021-10-08 17:54:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# 5287-A2
+# Assignment 2 - CS5287
+
+# Milestone 1
+
+For this milestone both of us downloaded Vagrant to our local machines. We then edited the Vagrantfile for configuration details. 
+
+Install Vagrant from: https://www.vagrantup.com/
+
+We download the clouds.yaml file from Chameleon Cloud and made sure this file was placed correctly in the new virtual machine. You can locate this file under KVM.Access.Download.
+
+The command `vagrant up` creates a virtual machine in VirtualBox and then automatically installs Ansible and runs an Ansible playbook. 
+
+A demo for this milestone can be found at: https://youtu.be/svjahpe6LWs
+
+## Distribution of Work 
+
+Parish edited the Vagrant files, and then we both met over Zoom to test and get the playbooks working. Alex then created documentation and a demo video. 
+
+
+
+
---
+++ kafka/consumer.py
@@ -0,0 +1,67 @@
+#
+#
+# Author: Aniruddha Gokhale
+# CS4287-5287: Principles of Cloud Computing, Vanderbilt University
+#
+# Created: Sept 6, 2020
+#
+# Purpose:
+#
+#    Demonstrate the use of Kafka Python streaming APIs.
+#    In this example, demonstrate Kafka streaming API to build a consumer.
+#
+
+import os   # need this for popen
+import time # for sleep
+from kafka import KafkaConsumer  # consumer of events
+import sys
+import json
+import config
+import couchdb
+
+# We can make this more sophisticated/elegant but for now it is just
+# hardcoded to the setup I have on my local VMs
+
+# acquire the consumer
+# (you will need to change this to your bootstrap server's IP addr)
+consumer = KafkaConsumer (bootstrap_servers=config.kafka_servers, api_version=(2,13,0))
+
+# subscribe to topic
+consumer.subscribe(topics=["ny", "chi"])
+
+# Variables for CouchDB
+
+# settings for couchdb
+couchdb_username = 'admin'
+couchdb_password = 'welcome'
+couchdb_database = 'cloud_class'
+
+# Connect to CouchDB
+couch_db = couchdb.Server(f"http://{couchdb_username}:{couchdb_password}@129.114.25.135:5984/")
+
+# Create Database - or access if already created
+
+try:
+    db = couch_db.create(couchdb_database)  # newly created
+except:
+    db = couch_db[couchdb_database]  # existing
+
+# we keep reading, printing and saving JSON file to CouchDB
+for msg in consumer:
+
+    # Desteralize data and print message
+    msg = json.loads(str(msg.value, "ascii"))
+    # Save JSON document to CouchDB
+    print(msg)
+    db.save(msg)
+
+# we are done. As such, we are not going to get here as the above loop
+# is a forever loop.
+consumer.close ()
+    
+
+
+
+
+
+
