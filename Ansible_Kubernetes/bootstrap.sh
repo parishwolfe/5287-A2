5550ef81800f7812f7527edb4c5562e2ea970393
Parish Wolfe <parishwolfe@gmail.com>
2021-10-30 13:20:43 -0400

    reconfigure repo for assignment3

---
+++ Ansible_Kubernetes/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Bootstrapping steps. Here we create needed directories on the guest
+mkdir -p ~/.ssh
+mkdir -p ~/.ansible
+mkdir -p ~/.config
+mkdir -p ~/.config/openstack
+mkdir -p ~/tasks
+mkdir -p ~/kafka
+mkdir -p ~/AnsibleOnly_Local_and_Cloud
+
