2d8f22cf686bd43f2ed14e10117673fc954eacf3
Parish Wolfe <parishwolfe@gmail.com>
2021-10-30 15:37:46 -0400

    pa2ms2 partial 1

---
+++ Ansible_Kubernetes/kubernetes_files/update_sources.sh
@@ -0,0 +1,14 @@
+# Install Sources for Kubernetes and Docker
+
+# install key and source for docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# install key and source for kubernetes
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt update
+
