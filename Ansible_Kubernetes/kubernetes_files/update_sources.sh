75fb60c92ff9fc8fa68f11c7e0583c46fbf90557
Parish Wolfe <parishwolfe@gmail.com>
2021-10-31 22:33:46 -0400

    Summary (required)

--- Ansible_Kubernetes/kubernetes_files/update_sources.sh
+++ Ansible_Kubernetes/kubernetes_files/update_sources.sh
@@ -10,5 +10,3 @@ echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/doc
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt update
-
