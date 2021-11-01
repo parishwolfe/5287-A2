eb9d4fa618fc035faf993cd91b234308c11d55c8
Parish Wolfe <parishwolfe@gmail.com>
2021-10-31 22:58:28 -0400

    update script, install kubernetes

--- Ansible_Kubernetes/kubernetes_files/update_sources.sh
+++ Ansible_Kubernetes/kubernetes_files/update_sources.sh
@@ -1,7 +1,7 @@
 # Install Sources for Kubernetes and Docker
 
 # install key and source for docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
